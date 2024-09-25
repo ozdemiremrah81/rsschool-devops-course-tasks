e7ae6dd2a69d6c689c417ab4915d691fbb897cab
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-09-25 21:23:37 +0300

    Create main.tf
    main.tf added

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraformstates-1"
+    key            = "state/terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "tf_lock"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-north-1"
+  access_key = "AKIATHVQLESRFC4KRUVV"
+  secret_key = "lBLE4iSmD1Is0+LR/7LgRKnGoezARiF6wVfh5EV/"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
