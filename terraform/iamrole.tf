0e63c0f2495757bcc416de5d2bf4acfbfab127ab
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-09-27 10:13:38 +0300

    updated the iam terraform file

---
+++ terraform/iamrole.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "GithubActionsRole2" {
+  name = "GithubActionsRole2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        #Sid    = ""
+        Principal: {
+                Federated: "arn:aws:iam::222634386594:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Condition: {
+                StringEquals: {
+                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+                },
+                StringLike: {
+                    "token.actions.githubusercontent.com:sub": "repo:ozdemiremrah81/*"
+                }
+            },
+    ]
+  })
+
+}
