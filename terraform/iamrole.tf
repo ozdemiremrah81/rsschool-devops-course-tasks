f0ab65068e234d9db38189a1f580e37af9279a89
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-09-27 19:28:18 +0300

    added if condition to only run if iamrole does not exist  iamrole.tf

--- terraform/iamrole.tf
+++ terraform/iamrole.tf
@@ -1,15 +1,17 @@
-resource "aws_iam_role" "GithubActionsRole" {
+data "aws_iam_role" "existing_role" {
   name = "GithubActionsRole"
+}
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
+resource "aws_iam_role" "GithubActionsRole" {
+  count = length(data.aws_iam_role.existing_role.id) == 0 ? 1 : 0
+
+  name = "GithubActionsRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRoleWithWebIdentity"
         Effect = "Allow"
-        #Sid    = ""
         Principal = {
           Federated = "arn:aws:iam::222634386594:oidc-provider/token.actions.githubusercontent.com"
         }
