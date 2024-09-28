e4278e616d060eee06b944d0a49e7e8c61647816
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-09-28 07:34:40 +0300

    tfvars file disabled, to check state update issue

--- terraform/backend-config.tfvars
+++
@@ -1,5 +0,0 @@
-bucket         = "terraformstates-1"
-key            = "state/terraform.tfstate"
-region         = "eu-north-1"
-encrypt        = true
-dynamodb_table = "tf_lock"
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "terraformstates-1"
+    key            = "state/terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "tf_lock"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
