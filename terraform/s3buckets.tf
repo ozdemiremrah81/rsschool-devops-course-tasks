5e3fc18861f68d3d9b4f157eed7e96aecce001a1
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-09-28 07:06:31 +0300

    if not exists condition added to s3bucket tf

--- terraform/s3buckets.tf
+++ terraform/s3buckets.tf
@@ -1,4 +1,8 @@
+data "aws_s3_bucket" "my_bucket" {
+  name = "bucket49394"
+}
 resource "aws_s3_bucket" "my_bucket" {
+  count = length(data.aws_s3_buck.my_bucket.id) == 0 ? 1 : 0
   bucket = "bucket49394"  # Ensure the bucket name is globally unique
   acl    = "private"  # Sets the access control list, e.g., private, public-read
 
