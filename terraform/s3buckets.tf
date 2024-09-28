65fd116b4b93c8f5efc865fe9c7078bc7c15f1bd
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-09-28 12:03:42 +0300

    Update s3buckets.tf
    removed the condition (if not exists) since it was failing without the resource creation completed.

--- terraform/s3buckets.tf
+++ terraform/s3buckets.tf
@@ -1,9 +1,4 @@
-data "aws_s3_bucket" "my_bucket" {
-  bucket = "bucket49394"
-}
-
 resource "aws_s3_bucket" "bucket49394" {
-  count = length(data.aws_s3_bucket.my_bucket.id) == 0 ? 1 : 0
   bucket = "bucket49394"  # Ensure the bucket name is globally unique
   acl    = "private"  # Sets the access control list, e.g., private, public-read
 
