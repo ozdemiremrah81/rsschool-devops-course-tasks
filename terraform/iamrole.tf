08eff4b28f3f76af316c21fd89ccc1ef38e65bca
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-09-28 12:34:07 +0300

    Update iamrole.tf
    conditional statement removed.

--- terraform/iamrole.tf
+++ terraform/iamrole.tf
@@ -1,10 +1,4 @@
-data "aws_iam_role" "existing_role" {
-  name = "GithubActionsRole2"
-}
-
 resource "aws_iam_role" "GithubActionsRole2" {
-  count = length(data.aws_iam_role.existing_role.id) == 0 ? 1 : 0
-
   name = "GithubActionsRole2"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
