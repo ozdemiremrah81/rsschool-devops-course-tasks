aa57f40126682c6e1bfdc7443bc2520ec60af997
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-10-12 11:26:20 +0300

    subnet name format synced with public ones

--- terraform/privatesubnets.tf
+++ terraform/privatesubnets.tf
@@ -1,9 +1,9 @@
-resource "aws_subnet" "app1_private_subnet1" {
+resource "aws_subnet" "app1_privatesubnet1" {
   vpc_id            = aws_vpc.app1_vpc.id
   cidr_block        = "10.2.1.0/24"
   availability_zone = "eu-north-1a"  # Change based on your region
   }
-  resource "aws_subnet" "app1_private_subnet2" {
+  resource "aws_subnet" "app1_privatesubnet2" {
   vpc_id            = aws_vpc.app1_vpc.id
   cidr_block        = "10.2.2.0/24"
   availability_zone = "eu-north-1b"  # Different AZ
