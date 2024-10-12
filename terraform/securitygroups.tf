c24cd5f01d655d25ae4a82ba7f2efe7f13507afd
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-10-12 19:29:54 +0300

    typo fixed in ipp address of my home

--- terraform/securitygroups.tf
+++ terraform/securitygroups.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["88.223.152.199/32"]  # Replace with your IP range (office/home IP)
+    cidr_blocks = ["88.233.152.199/32"]  # Replace with your IP range (office/home IP)
   }
 
   egress {
