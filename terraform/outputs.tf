a283e5c648da9d33ba0aa0468da3217067818d3e
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-10-12 16:31:06 +0300

    ec2, outputs, security groups has been added

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "bastion_public_ip" {
+  value = aws_instance.bastion_host.public_ip
+}
+
+output "private_instance1_private_ip" {
+  value = aws_instance.private_instance1.private_ip
+}
+
+output "private_instance2_private_ip" {
+  value = aws_instance.private_instance2.private_ip
+}
