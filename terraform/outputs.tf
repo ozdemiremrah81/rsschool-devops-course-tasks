64b5826b39075000f737a86bcfcf57ff382f7a11
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-10-20 10:56:48 +0300

    key generation steps removed, priviously created key will be used

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,11 +8,11 @@ output "k3s_instance1_public_ip" {
 
 # outputs.tf
 
-output "k3s_ssh_private_key" {
-  description = "Private key for SSH access to the k3s instance"
-  value       =  tls_private_key.k3s_key.private_key_pem
-  sensitive   = true
-}
+#output "k3s_ssh_private_key" {
+  #description = "Private key for SSH access to the k3s instance"
+  #value       =  tls_private_key.k3s_key.private_key_pem
+  #sensitive   = true
+#}
 
 output "kubeconfig_file" {
   description = "The path to the kubeconfig file to access the k3s cluster"
