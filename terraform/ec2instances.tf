4b03a7d1961ebf9330145cb6cd86700c15ff40db
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-10-20 10:53:23 +0300

    key generation steps removed, priviously created key will be used

--- terraform/ec2instances.tf
+++ terraform/ec2instances.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "bastion_host" {
   subnet_id     = aws_subnet.app1_publicsubnet1.id  # Launch in a public subnet
 
   # Use your key pair for SSH access
-  key_name      = "k3s-key"  # Use your key pair for SSH (optional)
+  key_name      = "app1_natgw_keypair"  # Use your key pair for SSH (optional)
 
   # Attach the public Security Group
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
@@ -23,7 +23,7 @@ resource "aws_instance" "k3s_instance1" {
   instance_type = var.instance_type              # Choose instance type
   subnet_id     = aws_subnet.app1_publicsubnet2.id  # Public subnet2
 
-  key_name      = "k3s-key"  # Use your key pair for SSH (optional)
+  key_name      = "app1_natgw_keypair"  # Use your key pair for SSH (optional)
 
   # Attach the private Security Group
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
@@ -40,12 +40,12 @@ resource "aws_instance" "k3s_instance1" {
               EOF
 
   # Attach the key pair to the instance for SSH access
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = tls_private_key.k3s_key.private_key_pem
-    host        = self.public_ip
-  }
+  #connection {
+    #type        = "ssh"
+    #user        = "ubuntu"
+    #private_key = tls_private_key.k3s_key.private_key_pem
+    #host        = self.public_ip
+  #}
    # Output the public IP of the EC2 instance after creation
   provisioner "local-exec" {
     command = "echo 'k3s server running on: ${self.public_ip}'"
@@ -55,10 +55,10 @@ resource "aws_instance" "k3s_instance1" {
 }
 
 # Output the private key to access the instance
-output "private_key" {
-  value     = tls_private_key.k3s_key.private_key_pem
-  sensitive = true
-}
+#output "private_key" {
+  #value     = tls_private_key.k3s_key.private_key_pem
+  #sensitive = true
+#}
 
 # Output the EC2 instance public IP
 output "instance_ip" {
--- terraform/keys.tf
+++ terraform/keys.tf
@@ -1,10 +1,10 @@
 # Generate an SSH key pair
-resource "tls_private_key" "k3s_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
+#resource "tls_private_key" "k3s_key" {
+  #algorithm = "RSA"
+  #rsa_bits  = 2048
+#}
 
-resource "aws_key_pair" "k3s_key_pair" {
-  key_name   = "k3s-key"
-  public_key = tls_private_key.k3s_key.public_key_openssh
-}
+#resource "aws_key_pair" "k3s_key_pair" {
+  #key_name   = "k3s-key"
+  #public_key = tls_private_key.k3s_key.public_key_openssh
+#}
