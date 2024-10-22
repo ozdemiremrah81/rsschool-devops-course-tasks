b59a812f2fa6ff93f0a1a2b7142359e1886ca435
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-10-22 05:22:10 +0300

    name argument removed from subnets due to an error. no such argument for this resource

--- terraform/privatesubnets.tf
+++ terraform/privatesubnets.tf
@@ -1,11 +1,9 @@
 resource "aws_subnet" "app1_privatesubnet1" {
-  name              = "app1_privatesubnet1"
   vpc_id            = aws_vpc.app1_vpc.id
   cidr_block        = var.private_subnet1_cidr
   availability_zone = var.AZ1a
   }
   resource "aws_subnet" "app1_privatesubnet2" {
-  name              = "app1_privatesubnet2"
   vpc_id            = aws_vpc.app1_vpc.id
   cidr_block        = var.private_subnet2_cidr
   availability_zone = var.AZ1b
