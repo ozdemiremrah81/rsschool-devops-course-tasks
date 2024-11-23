235e29c447856634e96c3c9ba441557f845ecf6c
Emrah Ozdemir <43759247+ozdemiremrah81@users.noreply.github.com>
2024-11-23 14:58:32 +0300

    allow temporary ssh access

--- terraform/securitygroups.tf
+++ terraform/securitygroups.tf
@@ -63,4 +63,11 @@ resource "aws_security_group" "k3s_sg" {
     protocol    = "tcp"
     cidr_blocks = [var.AllowedIP]
   }
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
